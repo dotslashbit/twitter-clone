a41b5faef8b2d4e1eba5761ac9fe3a2df7bd9b41
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-25 21:10:18 +0530

    refined the schema and uer data fetching

--- actions/createTweet.ts
+++ actions/createTweet.ts
@@ -7,7 +7,6 @@ import { revalidatePath } from "next/cache";
 export const createTweet = async (formData: FormData) => {
   const user = await currentUser();
   const username = user?.username;
-  const name = user?.firstName;
   if (!user) {
     throw new Error("You must be signed in to use this feature");
   }
@@ -15,7 +14,6 @@ export const createTweet = async (formData: FormData) => {
   const content = formData.get("tweet");
   await prisma.tweet.create({
     data: {
-      name: name as string,
       username: username as string,
       content: content as string,
     },
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,11 @@
 import { createTweet, getTweets } from "@/actions/createTweet";
+import { currentUser } from "@clerk/nextjs";
 import React from "react";
 
 const HomePage = async () => {
   const tweets = await getTweets();
+  const user = await currentUser();
+  console.log(user);
   return (
     <div className="flex flex-col items-center">
       <form
@@ -27,9 +30,17 @@ const HomePage = async () => {
           return (
             <li key={tweet.id} className="border-b-2 border-gray-200 py-2">
               <div>
-                <div className="flex items-center gap-5">
-                  <p>{tweet.name}</p>
-                  <p>@{tweet.username}</p>
+                <div className="flex items-center gap-1">
+                  <img
+                    src={user?.imageUrl}
+                    alt="image url"
+                    className="w-10 h-10 rounded-full"
+                  />
+
+                  <div className="flex items-center gap-5">
+                    <p>{`${user?.firstName} ${user?.lastName}`}</p>
+                    <p className="text-sm">@{tweet.username}</p>
+                  </div>
                 </div>
                 <p>{tweet.content}</p>
               </div>
