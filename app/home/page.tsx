6d2988bcef0d4a0b150d1a22715e9b157e342422
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-25 20:24:28 +0530

    redesigned tweet input form

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,16 +5,27 @@ const HomePage = async () => {
   const tweets = await getTweets();
   return (
     <div className="flex flex-col items-center">
-      <form action={createTweet}>
-        <input className="text-black" type="text" name="tweet" />
-        <button className="text-white" type="submit">
+      <form
+        action={createTweet}
+        className="border-blue-500 p-4 rounded-lg shadow-lg bg-white max-w-md w-full"
+      >
+        <input
+          className="text-black border-2 border-gray-300 p-2 w-full rounded-md mb-3"
+          type="text"
+          name="tweet"
+          placeholder="What is happening?"
+        />
+        <button
+          className="text-white bg-blue-500 hover:bg-blue-700 p-2 rounded-md w-full"
+          type="submit"
+        >
           Submit
         </button>
       </form>
-      <ul>
+      <ul className="mt-4 w-full max-w-md">
         {tweets.map((tweet) => {
           return (
-            <li key={tweet.id}>
+            <li key={tweet.id} className="border-b-2 border-gray-200 py-2">
               <div>
                 <p>{tweet.username}</p>
                 <p>{tweet.content}</p>
