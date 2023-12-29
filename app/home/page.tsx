a76b45219efac2a808f124a6f2fb00ea7387ec3b
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-29 16:15:58 +0530

    added navbar

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -29,13 +29,29 @@ const HomePage = async () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <TweetForm />
-      <ul className="mt-4 w-full max-w-md">
-        {tweets.map((tweet) => (
-          <TweetsList key={tweet.id} tweetId={tweet.id} />
-        ))}
-      </ul>
+    <div>
+      <nav className="flex items-center justify-between flex-wrap p-6">
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 300 300"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="white"
+            d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"
+          />
+        </svg>
+      </nav>
+
+      <div className="flex flex-col items-center">
+        <TweetForm />
+        <ul className="mt-4 w-full max-w-md">
+          {tweets.map((tweet) => (
+            <TweetsList key={tweet.id} tweetId={tweet.id} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
