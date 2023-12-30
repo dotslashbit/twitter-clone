0dcb0c0214b63b9b3fd98897d7bcde249be7d874
Sahil Mahapatra <sahilm231199@gmail.com>
2023-12-31 04:01:01 +0530

    fix: wrongly refactored createUser server action

--- actions/createUser.ts
+++ actions/createUser.ts
@@ -3,7 +3,7 @@
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs";
 
-export const createUser = async ({ id }: { id: string }) => {
+export const createUser = async (id: string) => {
   // Check if a user with the given ID already exists
   let user = await prisma.user.findUnique({
     where: { id },
