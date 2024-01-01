aa3c4df631e3ee272203877e9405d0b5517571a4
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-02 01:45:33 +0530

    fix: commented out the profile component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,12 +1,12 @@
-"use client";
-import React from "react";
-import { usePathname } from "next/navigation";
-const Profile = ({ users }) => {
-  const pathname = usePathname();
-  console.log("pathname", pathname);
-  const URLusername = pathname.split("/")[2];
-  const username = users.find((user) => user.username === URLusername);
-  return <div>Profile</div>;
-};
+// "use client";
+// import React from "react";
+// import { usePathname } from "next/navigation";
+// const Profile = ({ users }) => {
+//   const pathname = usePathname();
+//   console.log("pathname", pathname);
+//   const URLusername = pathname.split("/")[2];
+//   const username = users.find((user) => user.username === URLusername);
+//   return <div>Profile</div>;
+// };
 
-export default Profile;
+// export default Profile;
