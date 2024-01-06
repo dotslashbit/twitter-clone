20bd9e94c2faac6f27375cd34287c0958f89c913
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-06 09:49:41 +0530

    added app layout including signout feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignOutButton,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +24,39 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body>
+          <nav className="flex items-center justify-between flex-wrap p-6 bg-gray-800">
+            <Link href="/">
+              <svg
+                width="50"
+                height="50"
+                viewBox="0 0 300 300"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="white"
+                  d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"
+                />
+              </svg>
+            </Link>
+          </nav>
+          <div className="h-full">
+            <div className="flex">
+              <div className="w-64  bg-gray-800 text-white p-6 flex flex-col gap-5">
+                <Link href="/home">Explore</Link>
+                <Link href="/bookmarks">Bookmarks</Link>
+                <Link href="/profile">Profile</Link>
+                <div className="mt-auto">
+                  <UserButton />
+                </div>
+              </div>
+              <div className="flex-grow">
+                {/* Main content goes here */}
+                {children}
+              </div>
+            </div>
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
